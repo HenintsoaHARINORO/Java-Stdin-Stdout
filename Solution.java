79a1545209eed03d84d0969d5005c75f84581c6a
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2020-06-26 16:57:18 +0300

    keyboard buffer

--- Solution.java
+++ Solution.java
@@ -6,6 +6,7 @@ public class Solution {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
         double d= scan.nextDouble();
+        scan.nextLine();// after supplying data for double and integer,we should delete the keyboard buffer
         String s= scan.nextLine();
         scan.close();
         // Write your code here.
